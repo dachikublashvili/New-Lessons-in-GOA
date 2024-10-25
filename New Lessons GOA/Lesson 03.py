e56d3cb34bbd7ed4aa6939839da7171251c1d3ca
dachikublashvili <Dachman.kublashvili@gmail.com>
2024-10-25 21:54:44 +0400

    Third Commit

---
+++ New Lessons GOA/Lesson 03.py
@@ -0,0 +1,50 @@
+age = "9"
+print(age + "2") 
+#concatination - კონკატინაცია (ორი ან რამოდენიმე სტრინგის შეტყუპება (მიმატება))
+# + მიმატება
+# - გამოკლება
+# * გამრავლება
+# / გაყოფა
+# print(5 + 25) #30
+# print(25 - 5) #20
+# print(5 * 5) #25
+# print(25 / 5) #5.0 <--- გაყოფა ყოველთვის გვიბუნებს წილად რიცხვს
+# name = "group 51"
+# print(name * 100000)
+
+# dachi is a good boy 
+
+print(25+5)
+print(25-5)
+print(25*5)
+print(25/5)
+
+# დავალება 1
+name = " " "Dachi" " " "Kublashvili"
+print(name * 100)
+
+# დავალება 2
+age = "8"
+print (age + "1")
+
+
+
+
+# დავალება 3
+city = "London"
+print(city)
+
+
+print(22+22) #sophiko
+print(44-2) #dato
+print(1+1) #deme
+print(10-1) #dachi
+
+group = 51
+day = 3 
+print(group - day)
+print(group +  day)
+print(group /  day)
+print(group *  day)
+
+# ნიშნით - კომენტარი არ გამოდის ტერმინალში და იწერება იმისთვის, რომ გავიხსენოთ რა გავაკეთედ, მერე რომ დაგვჭირდება ადვილად გავიგოთ დავალების შინაარსი.
