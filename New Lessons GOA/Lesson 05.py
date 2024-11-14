05a3511e2709dc22b3a023d5a59987b29fdb0596
dachikublashvili <Dachman.kublashvili@gmail.com>
2024-11-15 01:12:53 +0400

    6th commit

---
+++ New Lessons GOA/Lesson 05.py
@@ -0,0 +1,15 @@
+name = input("my favorite sport is: ")
+print(name)
+
+
+name = input("what is your name: ")
+surname = input("what is your surname: ")
+age = int(input("what is your age: "))
+city = input("what is your city: ")
+hobby = input("your hobby: ")
+print(name, surname, age, city, hobby)
+
+number1 = int(input("first number: "))
+number2 = int(input("second number: "))
+print(number1+number2)
+print(number1/number2)
