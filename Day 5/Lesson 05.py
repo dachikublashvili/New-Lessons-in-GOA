f1504da662c1fbed7eea4d389800b620f1284d4f
dachikublashvili <Dachman.kublashvili@gmail.com>
2024-11-21 23:33:24 +0400

    seventh commit.

---
+++ Day 5/Lesson 05.py
@@ -0,0 +1,15 @@
+name = input("my favorite sport is: ") #დავალება 1
+print(name)
+
+
+name = input("what is your name: ") #დავალება 2
+surname = input("what is your surname: ")
+age = int(input("what is your age: "))
+city = input("what is your city: ")
+hobby = input("your hobby: ")
+print(name, surname, age, city, hobby)
+
+number1 = int(input("first number: ")) #დავალება 3
+number2 = int(input("second number: "))
+print(number1+number2)
+print(number1/number2)
