0fc7dbed02895480fd11bc02733a75c58cfd3634
dachikublashvili <Dachman.kublashvili@gmail.com>
2024-12-13 22:31:41 +0400

    10th commit

---
+++ Day 10/Classwork/main.py
@@ -0,0 +1,30 @@
+x = 5
+print(type(x))
+x = "5"
+print(type(x))
+
+a = "snow"
+b = "ball"
+print(a+b)
+
+# explicity მონაცემთა ტიპის შეცვლა ჩვენი ჩარევით
+number = 5
+number = str(5)
+print(type(number))
+
+# implicity მონაცემთა ტიპის შეცვლა ჩვენი ჩარევის გარეშე
+print(12/6)
+
+# True, False 
+print(5>2)
+print(5<2)
+print(10>2)
+print(60>20)
+print(55>1)
+print(50>5)
+print(10>1)
+print(11<1)
+print(10<6)
+print(60>90)
+print(80>98)
+print(98>698)
---
+++ Day 10/Homework/main.py
@@ -0,0 +1,34 @@
+print(5<1)
+print(10>100)
+print(20<5)
+print(52>53)
+print(102<95)
+print(1000>1111)
+print(200<102)
+print(522>625)
+print(20>200)
+print(525>785)
+print(100>50)
+print(700>600)
+print(500>200)
+print(200>100)
+print(100<1000)
+print(10>5)
+print(900>800)
+print(300<400)
+print(52>51)
+print(101<102)
+
+print(541>540)
+print(845>842)
+print(636>563)
+
+print(264>282)
+print(236<126)
+print(487>952)
+
+# explicity = მონაცემების დამუშავებაში ჩვენ ვახდენთ ჩარევას
+# imlicity = მონაცემების დამუშავება ხდება ჩვენი ჩარევის გარეშე
+
+number1 = input("please enter the number: ")
+print(number1)
